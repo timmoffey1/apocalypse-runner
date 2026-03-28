3e94264ed3e632a7863b653e3930738ebdb7bd5c
timmoffey1 <144036311+timmoffey1@users.noreply.github.com>
2026-03-28 09:44:33 -0400

    Create webpack.config.js for Phaser game development

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /.png$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                        }
+                    }
+                ]
+            },
+            // Add other loaders as necessary
+        ]
+    },
+    resolve: {
+        alias: {
+            phaser: path.resolve(__dirname, 'node_modules/phaser'),
+        }
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    plugins: [],
+};
